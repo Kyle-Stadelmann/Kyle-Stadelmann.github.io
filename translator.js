3cadc1e0f1b92075fb0337fcd9dcaa4182b1234c
Kyle Stadelmann <dlice@hotmail.com>
2020-04-01 22:28:48 -0700

    wrote translator

---
+++ translator.js
@@ -0,0 +1,78 @@
+function createCharMap() {
+	let charMap = new Map([
+		['a', 's']
+		['b', 'n']
+		['c', 'v']
+		['d', 'f']
+		['e', 'r']
+		['f', 'g']
+		['g', 'h']
+		['h', 'j']
+		['i', 'o']
+		['j', 'k']
+		['k', 'l']
+		['l', ';']
+		['m', ',']
+		['n', 'm']
+		['o', 'p']
+		['p', '[']
+		['q', 'w']
+		['r', 't']
+		['s', 'd']
+		['t', 'y']
+		['u', 'i']
+		['v', 'b']
+		['w', 'e']
+		['x', 'c']
+		['y', 'u']
+		['z', 'x']
+		['`', '1']
+		['1', '2']
+		['2', '3']
+		['3', '4']
+		['4', '5']
+		['5', '6']
+		['6', '7']
+		['7', '8']
+		['8', '9']
+		['9', '0']
+		['0', '-']
+		['-', '=']
+		['[', ']']
+		[']', '\']
+		[';', ''']
+		[',', '.']
+		['.', '/']
+	]);
+
+	return charMap;
+}
+
+reverseCharMap = new Map();
+function reverseMap(key, value, map) {
+	reverseCharMap[value] = key;
+}
+
+const charMap = createCharMap();
+charMap.forEach(reverseMap);
+
+
+function translateToScoot(str) {
+	return translate(str, charMap);
+}
+
+function translateFromScoot(str) {
+	return translate(str, reverseCharMap);
+}
+
+
+// function to translate a string using the map
+function translate(str, map) {
+	newStr = "";
+	for (let i = 0; i < str.length; i++) {
+		char curr = str.charAt(i);
+		newStr += map.get(curr);
+	}
+
+	return newStr;
+}
